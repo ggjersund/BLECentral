7008a87aeda266eb12c98f38be8c2679944cbf41
Gjert Ingar Gjersund <gjertig@stud.ntnu.no>
2019-03-01 00:52:15 +0100

    Make bluetooth adapter switch

--- src/core/adapter.py
+++ src/core/adapter.py
@@ -1,4 +1,20 @@
 """
 Setup the local Bluetooth adapter.
 Handle requests for turn on bluetooth, check version and if bluetooth is enabled etc.
+
+The Bluetooth adapter must be accessed via loadBunlde
+
 """
+
+import objc
+
+objc.loadBundleFunctions(
+    objc.loadBundle("IOBluetooth", globals(), bundle_path=objc.pathForFramework(u'/System/Library/Frameworks/IOBluetooth.framework')),
+    globals(),
+    [('IOBluetoothPreferenceGetControllerPowerState', b'oI'),('IOBluetoothPreferenceSetControllerPowerState', b'vI')]
+)
+
+# Turn bluetooth on
+IOBluetoothPreferenceSetControllerPowerState(1)
+
+# Turn bluetooth off
